96bb6476957c9f837008d87a9c5246460da608e3
Koo <koosco136@gmail.com>
2025-11-30 19:50:14 +0900

    config: move feign annotation to root application class

--- src/main/kotlin/com/koosco/userservice/UserServiceApplication.kt
+++ src/main/kotlin/com/koosco/userservice/UserServiceApplication.kt
@@ -2,8 +2,10 @@ package com.koosco.userservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients
 class UserServiceApplication
 
 fun main(args: Array<String>) {
