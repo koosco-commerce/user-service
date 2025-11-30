dd8bf56d997d07767e537e092f8b0547cea5c34b
Koo <koosco136@gmail.com>
2025-11-30 20:04:04 +0900

    fix: remove role from create user request in auth client

--- src/main/kotlin/com/koosco/userservice/infra/client/dto/AuthRequests.kt
+++ src/main/kotlin/com/koosco/userservice/infra/client/dto/AuthRequests.kt
@@ -1,14 +1,7 @@
 package com.koosco.userservice.infra.client.dto
 
 import com.koosco.userservice.domain.vo.AuthProvider
-import com.koosco.userservice.domain.vo.UserRole
 
-data class CreateUserRequest(
-    val userId: Long,
-    val email: String,
-    val password: String,
-    val provider: AuthProvider?,
-    val role: UserRole,
-)
+data class CreateUserRequest(val userId: Long, val email: String, val password: String, val provider: AuthProvider?)
 
 data class DeleteUserRequest(val userId: Long)
