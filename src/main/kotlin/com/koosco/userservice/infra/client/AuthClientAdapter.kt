8defcbf7541a8a3c1794f20d5685bd1462e577b6
Koo <koosco136@gmail.com>
2025-12-10 06:02:36 +0900

    feat: enhance AuthClientAdapter with dynamic auth service URL and logging

--- src/main/kotlin/com/koosco/userservice/infra/client/AuthClientAdapter.kt
+++ src/main/kotlin/com/koosco/userservice/infra/client/AuthClientAdapter.kt
@@ -7,10 +7,24 @@ import com.koosco.userservice.domain.enums.AuthProvider
 import com.koosco.userservice.domain.enums.UserRole
 import com.koosco.userservice.infra.client.dto.CreateUserRequest
 import feign.FeignException
+import jakarta.annotation.PostConstruct
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
-class AuthClientAdapter(private val authClient: AuthClient) : AuthServiceClient {
+class AuthClientAdapter(
+    private val authClient: AuthClient,
+    @Value("\${auth-service.url}")
+    private val authServiceUrl: String,
+) : AuthServiceClient {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    @PostConstruct
+    fun init() {
+        logger.info("AuthClient initialized with URL: $authServiceUrl")
+    }
 
     override fun notifyUserCreated(
         userId: Long,
